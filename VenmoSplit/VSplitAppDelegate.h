6b90f6090ba86857211f228fe9c97b9f7178c477
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-02-10 20:06:44 -0500

    Initial Commit

---
+++ VenmoSplit/VSplitAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VSplitAppDelegate.h
+//  VenmoSplit
+//
+//  Created by Basil Siddiqui on 2/10/13.
+//  Copyright (c) 2013 Basil Siddiqui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VSplitAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VenmoSplit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VenmoSplit
+//
+//  Created by Basil Siddiqui on 2/10/13.
+//  Copyright (c) 2013 Basil Siddiqui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VSplitAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VSplitAppDelegate class]));
+    }
+}
